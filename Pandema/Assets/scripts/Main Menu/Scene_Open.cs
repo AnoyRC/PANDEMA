8268763d472a8e45b7f9f2b587e96ee4e4377869
ANOY ROY CHOWDHURY <anoyrc@outlook.com>
2018-05-02 17:31:13 +0530

    MainMenu updated

---
+++ Pandema/Assets/scripts/Main Menu/Scene_Open.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Open : MonoBehaviour {
+    public Animator anim;
+    public string NameOfScene;
+public void Scene()
+    {
+        anim.SetBool("1", true);
+        StartCoroutine(WAIT());
+    }
+    public IEnumerator WAIT()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(NameOfScene);
+    }
+}
