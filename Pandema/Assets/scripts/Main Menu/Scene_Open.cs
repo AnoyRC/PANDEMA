a46993ff41d4ef6fc50db911ecf2e1446584bd33
ShambaC <38806897+ShambaC@users.noreply.github.com>
2018-05-03 19:50:51 +0530

    best main menu

--- Pandema/Assets/scripts/Main Menu/Scene_Open.cs
+++ Pandema/Assets/scripts/Main Menu/Scene_Open.cs
@@ -6,7 +6,38 @@ using UnityEngine.SceneManagement;
 public class Scene_Open : MonoBehaviour {
     public Animator anim;
     public string NameOfScene;
-public void Scene()
+    public GameObject[] night;
+    public GameObject[] morn;
+    public GameObject[] sunset;
+    private int i = 0;
+    private int syshour;
+
+    public void Start()
+    {
+        syshour = System.DateTime.Now.Hour;
+        if(syshour>8 && syshour<14)
+        {
+            for(i=0;i<morn.Length;i++)
+            {
+                morn[i].gameObject.SetActive(true);
+            }
+        }
+        if (syshour > 14 && syshour < 19)
+        {
+            for (i = 0; i < sunset.Length; i++)
+            {
+                sunset[i].gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            for (i = 0; i < night.Length; i++)
+            {
+                night[i].gameObject.SetActive(true);
+            }
+        }
+    }
+    public void Scene()
     {
         anim.SetBool("1", true);
         StartCoroutine(WAIT());
